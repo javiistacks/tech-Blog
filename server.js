86e107074a1d340aa41f751196b03500a9489b42
Javiistacks <javiann.marrero@icloud.com>
2021-05-06 21:35:18 -0500

    added cookie session time

--- server.js
+++ server.js
@@ -14,3 +14,23 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'bigredcat',
+    cookie: {
+        //session will expire in 10 minutes
+        expires: 10 * 60 * 1000
+},
+resave: true,
+saveUninitialized: true,
+rolling:true,
+store: new SequelizeStore({
+    db: sequelize
+}),
+};
+
+
+
+
+
+}
