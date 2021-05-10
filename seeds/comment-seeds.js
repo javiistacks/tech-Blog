253fd65f7f609735f4923c89b025ab822d331718
Javiistacks <javiann.marrero@icloud.com>
2021-05-09 20:57:06 -0500

    added blogs and comments

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -4,42 +4,42 @@ const commentData = [
     {
         user_id: 1,
         post_id: 5,
-        comment_text: ""
+        comment_text: "Im ready to invest!"
     },
     {
         user_id: 4,
         post_id: 4,
-        comment_text: ""
+        comment_text: "I would go only if I can get back alive."
     },
     {
         user_id: 1,
         post_id: 4,
-        comment_text: ""
+        comment_text: "No way jose!"
     },
     {
         user_id: 3,
         post_id: 5,
-        comment_text: ""
+        comment_text: "ALL the Marvel movies."
     },
     {
         user_id: 3,
         post_id: 2,
-        comment_text: ""
+        comment_text: "I love space!"
     },
     {
         user_id: 3,
         post_id: 4,
-        comment_text: ""
+        comment_text: "When are we leaving?"
     },
     {
         user_id: 5,
         post_id: 3,
-        comment_text: ""
+        comment_text: "I have a Tesla and let me tell you it is the best car ever."
     },
     {
         user_id: 2,
         post_id: 1,
-        comment_text: ""
+        comment_text: "Im ready to invest!"
     }
 ]
 
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,29 +2,29 @@ const { Post } = require('../models');
 
 const postData = [
     {
-        title: "Example TITLE",
-        post_content: "CONTENT OF TECH BLOG GOES IN HERE ",
+        title: "Would you invest on DOGECOIN?",
+        post_content: "I know you all saw Elon Musk on SNL as he proclaimed himself the DogeFather. Is this enough for you to invest lest talk about it."
         user_id: 3
     },
     {
-        title: "Example TITLE",
-        post_content: "CONTENT OF TECH BLOG GOES IN HERE ",
+        title: "Astronomy",
+        post_content: "I wanted to start a thread to be able to talk about anything and everything about space.",
         user_id: 1
     },
     {
-        title: "Example TITLE",
-        post_content: "CONTENT OF TECH BLOG GOES IN HERE ",
+        title: "Cars",
+        post_content: "Tesla, Ferrari, Porsche, Ford, anything with 4 wheels we appreciate.",
         user_id: 2
 
     },
     {
-        title: "Example TITLE",
-        post_content: "CONTENT OF TECH BLOG GOES IN HERE ",
+        title: "MARS",
+        post_content: "Would you be able to move to mars and live there?",
         user_id: 5
     },
     {
-title: "Example TITLE",
-        post_content: "CONTENT OF TECH BLOG GOES IN HERE ",
+        title: "What's your favorite geek movie?",
+        post_content: "Matrix is the best! ",
         user_id: 4
     }
 ]
